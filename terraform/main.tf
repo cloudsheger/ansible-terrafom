dc6c3c8d1265c308675a77a7f5ada024353807cb
cloudsheger <cloudsheger@gmail.com>
2022-12-17 00:14:23 -0500

    parametrize terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,26 +1,26 @@
 resource "aws_instance" "this" {
-    ami           = data.aws_ami.this.id
-    instance_type = var.instance_type
-    key_name      = var.key
-    user_data     = data.template_file.init.template
-    #subnet_id     = data.aws_subnet.this.id
-    #map_public_ip_on_launch = true
+  ami           = data.aws_ami.this.id
+  instance_type = var.instance_type
+  key_name      = var.key
+  user_data     = data.template_file.init.rendered
+  #subnet_id     = data.aws_subnet.this.id
+  #map_public_ip_on_launch = true
 
-    #iam_instance_profile   = data.aws_iam_instance_profile.this.name
-    #vpc_security_group_ids = [data.aws_security_group.this.id]
+  #iam_instance_profile   = data.aws_iam_instance_profile.this.name
+  #vpc_security_group_ids = [data.aws_security_group.this.id]
 
-    root_block_device {
-        delete_on_termination = true
-        encrypted             = false
-        volume_size           = 160
-        volume_type           = "gp2"
-    }
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    volume_size           = 160
+    volume_type           = "gp2"
+  }
 
-    tags = {
-      "Name" =  "Developer Workstation for ${var.user_name}"
-      "Environment" = "Cloudsheger"
-      "Username"     = var.user_name
-      "clap_on"    = "0 4 @ @ 1-5 @"
-      "clap_off"   = "20:00"
-    }
+  tags = {
+    "Name"        = "Developer Workstation for ${var.user_name}"
+    "Environment" = "Cloudsheger"
+    "Username"    = title(var.user_name)
+    "clap_on"     = "0 4 @ @ 1-5 @"
+    "clap_off"    = "20:00"
+  }
 }
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,9 +1,9 @@
 terraform {
-    backend "s3" {
-        bucket = "workstation-repo"
-        key    = "workstation-repo/workstation"
+  backend "s3" {
+    bucket = "workstation-repo"
+    key    = "workstation-repo/workstation"
 
-        region = "us-east-1"
-        dynamodb_endpoint = "workstation"
-    }
+    region            = "us-east-1"
+    dynamodb_endpoint = "workstation"
+  }
 }
